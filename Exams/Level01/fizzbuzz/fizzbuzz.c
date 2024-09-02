08c9ed1d6d7ee5582cf116767d03d78f8ffea0a0
Lucasuriasl <122507938+Lucasuriasl@users.noreply.github.com>
2024-09-02 08:59:52 +0100

    Update fizzbuzz.c
    tentativa com printf apenas para testar uma função

--- Exams/Level01/fizzbuzz/fizzbuzz.c
+++ Exams/Level01/fizzbuzz/fizzbuzz.c
@@ -1,41 +1,40 @@
 //QUESTAO FIZZBUZZ 42 EXAME LEVEL01
-// printa de 1 a 100, se for multiplo de 3 printa fizz, se for multiplo  
+// printa de 1 a 100, se for multiplo de 3 printa fizz, se for multiplo
 // de 5 printa buzz, se for dos dois printa fizzbuzz. Bora!
 
 #include<unistd.h>
 #include <stdio.h>
 
-
 int main ()
 
 {
-	int	i;	
-	//char	temp;
-	
-	i = 0;
-	//temp = i;
+        int     i;
+        //char  temp;
+
+        i = 0;
+        //temp = i;
 
 
-	while ( i >= '0' || i <= '100')
-	{
-		i++;
-		
+        while ( i <= 100)
+        {
+                i++;
 
-		if ( i % 3 == 0 ) 
-		write ( 1, "fizz\n", 6);
 
-		if ( i % 5 == 0)
-		write (1, "buzz\n", 6);
-		
-		if ( i % 3 == 0 && i % 5 == 0 )
+                if ( i % 3 == 0 )
+                write ( 1, "fizz\n", 6);
+
+                if ( i % 5 == 0)
+                write (1, "buzz\n", 6);
+
+                if ( i % 3 == 0 && i % 5 == 0 )
                 write (1, "fizzbuzz\n", 9 );
 
-		else 
-		
-		write (1, i, 3);
+                else
 
-		
-	}
+                //write (1, i, 3);
+                printf("%d\n", i);
+
+        }
 
 
 }
