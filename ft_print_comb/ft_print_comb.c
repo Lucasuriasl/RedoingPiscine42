b3b71177178d8d796ff5cff413b8a8ddc6b2eadd
Lucas Nascimento <122507938+Lucasuriasl@users.noreply.github.com>
2024-09-05 23:33:41 +0100

    Update ft_print_comb.c

---
+++ ft_print_comb/ft_print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<unistd.h>
+
+
+{
+        int     i;
+        int     j;
+        int     k;
+
+
+        i = 0;
+
+        while ( i < 0)
+
+
+
+
+}
+
+int     main()
+{
+
+
+
+}
